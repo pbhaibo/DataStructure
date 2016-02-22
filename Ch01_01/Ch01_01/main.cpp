dce8c7d970851c7bf733ca73d9aab789016a9268
pbhaibo <pbhaibo@126.com>
2016-02-22 21:17:25 +0800

    update

--- Ch01_01/Ch01_01/main.cpp
+++ Ch01_01/Ch01_01/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  test
+//  Ch01_01
 //
 //  Created by PengBo on 16/1/31.
 //  Copyright © 2016年 PengBo. All rights reserved.
@@ -9,30 +9,40 @@
 #include <iostream>
 #include <time.h>
 #include "Sort.h"
+#include <ctime>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     //random generate an array
-    int N = 100;
-    unsigned int *pArray = new unsigned int[N];
-    srand(time(NULL));
-    for (int i=0; i<N; i++)
+    clock_t cStart_t, cEnd_t;
+    for (int N=1; N<20001; N++)  //set N from 0~20000
     {
-        pArray[i] = rand()%(N+1);
-        // cout<<dArray[i]<<endl;
+        cStart_t = clock();
+        unsigned int *pArray = new unsigned int[N];
+        srand(time(NULL));
+        for (int i=0; i<N; i++)
+        {
+            pArray[i] = rand()%(N+1);
+            // cout<<dArray[i]<<endl;
+        }
+        
+        //select
+        int k = N / 2 ;
+        BubbleSort(pArray, N);  //sort the array pArray
+        
+ /*       for (int i=0 ; i<N; i++) {
+            cout<<pArray[i]<<endl;
+        }*/
+        
+//        cout<<"The Kth greatest number is "<<pArray[k]<<endl;
+        delete[] pArray;
+        
+        cEnd_t = clock();
+        cout<<(cEnd_t-cStart_t)/1000<<endl;
     }
     
-    //select
-    int k = N / 2 ;
-    BubbleSort(pArray, N);  //sort the array pArray
     
-    for (int i=0 ; i<N; i++) {
-        cout<<pArray[i]<<endl;
-    }
-    
-    cout<<"The Kth greatest number is "<<pArray[k]<<endl;
-    
-    delete[] pArray;
+ 
     return 0;
 }
