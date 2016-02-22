d9698798c258ef374163c7b630e687622b8b271e
pbhaibo <pbhaibo@126.com>
2016-02-22 21:19:43 +0800

    create Ch01_02

---
+++ Ch01_02/Ch01_02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ch01_02
+//
+//  Created by PengBo on 16/2/22.
+//  Copyright © 2016年 PengBo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
