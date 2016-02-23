cccb085cbc55b926c08fd2ad09cda895c9f30e66
pbhaibo <pengbo@radi.ac.cn>
2016-02-23 11:55:44 +0800

    update FindWords.h and main.cpp

--- Ch01_02/Ch01_02/FindWords.h
+++ Ch01_02/Ch01_02/FindWords.h
@@ -16,120 +16,104 @@ using namespace std;
 //已有的函数，查阅资料
 void MakeRightWord(string &sMadeWord, char *pElemt, int nSize)
 {
-    char* pWord = new char[nSize];
+	sMadeWord = "";
     for (int i=0; i<nSize; i++)
     {
-        pWord[i] = pElemt[i];
+		sMadeWord += pElemt[i];
     }
-    sMadeWord = pWord;
-    delete[] pWord;
 }
 
 void MakeLeftWord(string &sMadeWord, char *pElemt, int nSize)
 {
-    char* pWord = new char[nSize];
+    sMadeWord = "";
     for (int i=0; i<nSize; i++)
     {
-        pWord[i] = *(pElemt-i);
+        sMadeWord += *(pElemt-i);
     }
-    sMadeWord = pWord;
-    delete[] pWord;
 }
 
 void MakeUpWord(string &sMadeWord, char *pElemt, int nSize)
 {
-    char* pWord = new char[nSize];
+    sMadeWord = "";
     for (int i=0; i<nSize; i++)
     {
-        pWord[i] = *(pElemt-i*4);
+        sMadeWord += *(pElemt-i*4);
     }
-    sMadeWord = pWord;
-    delete[] pWord;
 }
 
 void MakeDownWord(string &sMadeWord, char *pElemt, int nSize)
 {
-    char* pWord = new char[nSize];
+    sMadeWord = "";
     for (int i=0; i<nSize; i++)
     {
-        pWord[i] = *(pElemt+i*4);
+        sMadeWord += *(pElemt+i*4);
     }
-    sMadeWord = pWord;
-    delete[] pWord;
 }
 
 
 //for char located where row equals to col
 void MakeRightDownWord(string &sMadeWord, char *pElemt, int nSize)
 {
-    char* pWord = new char[nSize];
+    sMadeWord = "";
     for (int i=0; i<nSize; i++)
     {
-        pWord[i] = *(pElemt+i*4+i);
+        sMadeWord += *(pElemt+i*4+i);
     }
-    sMadeWord = pWord;
-    delete[] pWord;
 }
 
 void MakeRightUpWord(string &sMadeWord, char *pElemt, int nSize)
 {
-    char* pWord = new char[nSize];
+    sMadeWord = "";
     for (int i=0; i<nSize; i++)
     {
-        pWord[i] = *(pElemt-i*4+i);
+        sMadeWord += *(pElemt-i*4+i);
     }
-    sMadeWord = pWord;
-    delete[] pWord;
 }
 
 void MakeLeftDownWord(string &sMadeWord, char *pElemt, int nSize)
 {
-    char* pWord = new char[nSize];
+    sMadeWord = "";
     for (int i=0; i<nSize; i++)
     {
-        pWord[i] = *(pElemt+i*4-i);
+        sMadeWord += *(pElemt+i*4-i);
     }
-    sMadeWord = pWord;
-    delete[] pWord;
 }
 
 void MakeLeftUpWord(string &sMadeWord, char *pElemt, int nSize)
 {
-    char* pWord = new char[nSize];
+    sMadeWord = "";
     for (int i=0; i<nSize; i++)
     {
-        pWord[i] = *(pElemt-i*4+i);
+        sMadeWord += *(pElemt-i*4-i);
     }
-    sMadeWord = pWord;
-    delete[] pWord;
 }
 
 
 ////match the word with the words in the list
-string WordMatch(string sWord, string* pWordList, int nListSize)
+bool WordMatch(string sWord, string* pWordList, int nListSize)
 {
     for (int i=0; i<nListSize; i++) {
         if (sWord == pWordList[i])
         {
-            return sWord;
+            return 1;
         }
     }
-    return string("null");
+    return 0;
 }
 
 void FindWords(vector<string> &sFindList, string *pWordList, char* pCh, int nSize)
 {
-    string sWordMade = "null";
+    string sWordMade = "";
     for (int i=0; i<nSize; i++) {
         for (int j=0; j<nSize; j++) {
             //locate the pointer to &pCh[i][j] = pCh+i*nSize+j
-                //search right
+            //search right
             for (int k=2; k<=nSize-j; k++) //k is the number of letters in a word
             {
                 //make up a word
                 MakeRightWord(sWordMade, pCh+i*nSize+j, k);
                 //match the word with the words in the list
-                if (WordMatch(sWordMade, pWordList, 4)!= "null")
+                if (WordMatch(sWordMade, pWordList, 4))
                 {
                     //cout<<sWordMade<<'\t';
                     sFindList.push_back(sWordMade);
@@ -139,7 +123,7 @@ void FindWords(vector<string> &sFindList, string *pWordList, char* pCh, int nSiz
             for (int k=2; k<=j+1; k++)
             {
                 MakeLeftWord(sWordMade, pCh+i*nSize+j, k);
-                if (WordMatch(sWordMade, pWordList, 4)!= "null")
+                if (WordMatch(sWordMade, pWordList, 4))
                 {
                     //cout<<sWordMade<<'\t';
                     sFindList.push_back(sWordMade);
@@ -150,7 +134,7 @@ void FindWords(vector<string> &sFindList, string *pWordList, char* pCh, int nSiz
             for (int k=2; k<=i+1; k++)
             {
                 MakeUpWord(sWordMade, pCh+i*nSize+j, k);
-                if (WordMatch(sWordMade, pWordList, 4)!= "null")
+                if (WordMatch(sWordMade, pWordList, 4))
                 {
                     //cout<<sWordMade<<'\t';
                     sFindList.push_back(sWordMade);
@@ -163,43 +147,54 @@ void FindWords(vector<string> &sFindList, string *pWordList, char* pCh, int nSiz
                 //make up a word
                 MakeDownWord(sWordMade, pCh+i*nSize+j, k);
                 //match the word with the words in the list
-                if (WordMatch(sWordMade, pWordList, 4)!= "null")
+                if (WordMatch(sWordMade, pWordList, 4))
                 {
                    // cout<<sWordMade<<'\t';
                     sFindList.push_back(sWordMade);
                 }
             }
             // if i==j, the diagonal should be considered
-            if (i==j) {
+            if (i==j || (nSize-i-1)==j) {
                 for (int k=2; k<=nSize-i; k++)
                 {
+					if ((nSize-j) < 2)
+					{
+						break;
+					}
                     //make up a word
                     MakeRightDownWord(sWordMade, pCh+i*nSize+j, k);
                     //match the word with the words in the list
-                    if (WordMatch(sWordMade, pWordList, 4)!= "null")
+                    if (WordMatch(sWordMade, pWordList, 4))
                     {
                        // cout<<sWordMade<<'\t';
                         sFindList.push_back(sWordMade);
                     }
                 }
-                for (int k=2; k<=nSize-i; k++)
+                for (int k=2; k<=i+1; k++)
                 {
+					if ((nSize-j) < 2)
+					{
+						break;
+					}
                     //make up a word
                     MakeRightUpWord(sWordMade, pCh+i*nSize+j, k);
                     //match the word with the words in the list
-                    if (WordMatch(sWordMade, pWordList, 4)!= "null")
+                    if (WordMatch(sWordMade, pWordList, 4))
                     {
                        // cout<<sWordMade<<'\t';
                         sFindList.push_back(sWordMade);
-
                     }
                 }
-                for (int k=2; k<=nSize-i; k++)
+                for (int k=2; k<=i+1; k++)
                 {
+					if ((j+1) < 2)
+					{
+						break;
+					}
                     //make up a word
                     MakeLeftUpWord(sWordMade, pCh+i*nSize+j, k);
                     //match the word with the words in the list
-                    if (WordMatch(sWordMade, pWordList, 4)!= "null")
+                    if (WordMatch(sWordMade, pWordList, 4))
                     {
                         //cout<<sWordMade<<'\t';
                         sFindList.push_back(sWordMade);
@@ -208,10 +203,14 @@ void FindWords(vector<string> &sFindList, string *pWordList, char* pCh, int nSiz
                 }
                 for (int k=2; k<=nSize-i; k++)
                 {
+					if ((j+1) < 2)
+					{
+						break;
+					}
                     //make up a word
                     MakeLeftDownWord(sWordMade, pCh+i*nSize+j, k);
                     //match the word with the words in the list
-                    if (WordMatch(sWordMade, pWordList, 4)!= "null")
+                    if (WordMatch(sWordMade, pWordList, 4))
                     {
                       //  cout<<sWordMade<<'\t';
                         sFindList.push_back(sWordMade);
--- Ch01_02/Ch01_02/main.cpp
+++ Ch01_02/Ch01_02/main.cpp
@@ -27,6 +27,7 @@ int main(int argc, const char * argv[]) {
                         'f', 'g', 'd', 't'};
     
     //print out pChar
+	cout<<"The words riddle is \n";
     for (int i=0; i<4; i++) {
         for (int j=0; j<4; j++) {
             cout<<pCh[i][j]<<'\t';
@@ -38,9 +39,10 @@ int main(int argc, const char * argv[]) {
     
     FindWords(sFindWordList, sWordList, &pCh[0][0], 4);
     
+	cout<<"\nThe words found are as follows: ";
     int nListSize = sFindWordList.size();
     for (int i=0; i<nListSize; i++) {
-        cout<<sFindWordList[i]<<'\t';
+        cout<<'['<<sFindWordList[i]<<']'<<' ';
     }
     
     cout<<endl;
